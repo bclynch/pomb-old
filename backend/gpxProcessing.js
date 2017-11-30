ad996d207ca64f6ac3be5a60e115ba07c48beeb7
bclynch <bclynch7@gmail.com>
2017-11-30 15:51:23 -0800

    gpx reporting work

--- backend/gpxProcessing.js
+++ backend/gpxProcessing.js
@@ -3,12 +3,13 @@ fs = require('fs'),
 geolib = require('geolib'),
 DOMParser = require('xmldom').DOMParser,
 multer = require('multer'),
+tempFolderPath = './tempFiles/',
 express = require('express'),
 router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './temp/')
+    cb(null, tempFolderPath)
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -23,12 +24,15 @@ var converted = convertGPX.gpx(gpx);
 
 // console.log(JSON.stringify(converted));
 
+let convertedTimeArr = [];
+let distanceArr = [];
+
 //Route 
 router.post("/", upload.array("uploads[]", 5), (req, res) => {
   const files = req.files;
   const name = files[0].originalname;
 
-  var gpx = new DOMParser().parseFromString(fs.readFileSync(`./temp/${name}`, 'utf8'));
+  var gpx = new DOMParser().parseFromString(fs.readFileSync(`${tempFolderPath}${name}`, 'utf8'));
   var converted = convertGPX.gpx(gpx);
 
   const totalDistance = getTotalDistance(converted.features[0].geometry.coordinates);
@@ -38,9 +42,17 @@ router.post("/", upload.array("uploads[]", 5), (req, res) => {
   const speeds = getSpeedArr(converted.features[0].geometry.coordinates, converted.features[0].properties.coordTimes);
   converted.features[0].speedsArr = speeds;
 
-  fs.unlink(`./temp/${name}`, () => console.log('deleted temp file')); 
+  converted.timeArr = convertedTimeArr.slice(0);
+  convertedTimeArr = [];
 
-  res.send(JSON.stringify({data: converted}));
+  converted.distanceArr = distanceArr.slice(0);
+  distanceArr = [];
+
+  fs.unlink(`${tempFolderPath}${name}`, () => {
+    console.log('deleted temp file');
+
+    res.send(JSON.stringify({data: converted}));
+  }); 
 });
 
 //take in coord arr and find total distance
@@ -49,26 +61,37 @@ function getTotalDistance(arr) {
 
   for(let i = 0; i < arr.length; i++) {
     if(i < arr.length - 1) {
-      totalDistance += geolib.getDistance(
+      const distance = geolib.getDistance(
         { latitude: arr[i][1], longitude: arr[i][0] },
         { latitude: arr[i + 1][1], longitude: arr[i + 1][0] }
       );
+
+      totalDistance += distance;
+      distanceArr.push(totalDistance);
     }
   }
+  //copying the last elem and pushing to end of arr so it has same count as coord arr.
+  distanceArr.splice(distanceArr.length - 1, 0, distanceArr[distanceArr.length - 1]);
 
   return totalDistance;
 }
 
 function getSpeedArr(geoArr, timeArr) {
   let speedsArr = [];
+  console.log(Date.parse(timeArr[0]));
   for(let i = 0; i < geoArr.length; i++) {
     if(i < geoArr.length - 1) {
+      const timeToMs = Date.parse(timeArr[i]);
+      convertedTimeArr.push(timeToMs);
       speedsArr.push(geolib.getSpeed(
-        { lat: geoArr[i][1], lng: geoArr[i][0], time: Date.parse(timeArr[i]) },
+        { lat: geoArr[i][1], lng: geoArr[i][0], time: timeToMs },
         { lat: geoArr[i + 1][1], lng: geoArr[i + 1][0], time: Date.parse(timeArr[i + 1]) }
       ));
     }
   }
+  //copying the last elem and pushing to end of arr so it has same count as coord arr. Same for times
+  speedsArr.splice(speedsArr.length - 1, 0, speedsArr[speedsArr.length - 1]);
+  convertedTimeArr.splice(convertedTimeArr.length - 1, 0, convertedTimeArr[convertedTimeArr.length - 1])
 
   return speedsArr;
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -14,7 +14,7 @@
 
 // }
 
-import { ViewChild, Component, ElementRef, AfterViewInit } from '@angular/core';
+import { ViewChild, Component, ElementRef } from '@angular/core';
 import { AgmMap, AgmDataLayer } from '@agm/core';
 import { SettingsService } from '../../services/settings.service';
 import { BroadcastService } from '../../services/broadcast.service';
@@ -26,9 +26,11 @@ import Chart from 'chart.js';
  selector: 'page-settings',
  templateUrl: 'settings.html'
 })
-export class SettingsPage implements AfterViewInit {
-  @ViewChild('donut') donut: ElementRef;
-  @ViewChild('line') line: ElementRef;
+export class SettingsPage {
+  @ViewChild('elevationTime') elevationTime: any;
+  @ViewChild('elevationDistance') elevationDistance: any;
+  @ViewChild('speedTime') speedTime: any;
+  @ViewChild('speedDistance') speedDistance: any;
 
   lat: number = 37.662323;
   lng: number = -122.399451;
@@ -38,51 +40,17 @@ export class SettingsPage implements AfterViewInit {
   filesToUpload: Array<File> = [];
 
   geoJsonObject: Object = null;
-  lineData: number[];
-
+  elevationTimeData: number[];
+  elevationDistanceData: number[];
+  speedTimeData: number[];
+  speedDistanceData: number[];
+ 
   constructor(
     private settingsService: SettingsService,
     private broadcastService: BroadcastService,
     private apiService: APIService
   ) {  
     this.settingsService.appInited ? console.log('neat') : this.broadcastService.on('appIsReady', () => console.log('neat'));
-  }
- 
-  ngAfterViewInit() {
-    let donutCtx = this.donut.nativeElement.getContext('2d');
-
-    var data = {
-      labels: [
-        "Value A",
-        "Value B"
-      ],
-      datasets: [
-        {
-          "data": [101342, 55342],   // Example data
-          "backgroundColor": [
-              "#1fc8f8",
-              "#76a346"
-          ]
-        }
-      ]
-    };
-
-    var chart = new Chart(
-      donutCtx,
-      {
-        "type": 'doughnut',
-        "data": data,
-        "options": {
-          "cutoutPercentage": 50,
-          "animation": {
-            "animateScale": true,
-            "animateRotate": false
-          }
-        } 
-      }
-    );
-    
-    this.createLineChart();
   }
 
   clicked(clickEvent) {
@@ -98,65 +66,49 @@ export class SettingsPage implements AfterViewInit {
   }
 
   createLineChart() {
-    let lineCtx = this.line.nativeElement.getContext('2d');
+    let elevationTimeCtx = this.elevationTime.nativeElement.getContext('2d');
     
-    // const mydata = {
-    //   labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-    //   datasets: [{ 
-    //       data: this.lineData,
-    //       label: "Africa",
-    //       borderColor: "#3e95cd", 
-    //       fill: false
-    //     }
-    //   ]
-    // }; 
+    console.log(this.elevationTimeData);
     const mydata = {
-      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
       datasets: [{ 
-          data: [86,114,106,106,107,111,133,221,783,2478],
-          label: "Africa",
-          lineTension: 0,
+          data: this.elevationTimeData,
+          label: "Elevation",
           backgroundColor: 'rgba(255,133,0,0.2)',
           borderColor: 'rgba(255,133,0,1)',
           pointBackgroundColor: 'rgba(255,133,0,1)',
           pointBorderColor: 'rgba(255,133,0,1)',
           pointHoverBackgroundColor: '#fff',
           pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        }, { 
-          data: [282,350,411,502,635,809,947,1402,3700,5267],
-          label: "Asia",
-          borderColor: "#8e5ea2",
-          fill: false
-        }, { 
-          data: [168,170,178,190,203,276,408,547,675,734],
-          label: "Europe",
-          borderColor: "#3cba9f",
-          fill: false
-        }, { 
-          data: [40,20,10,16,24,38,74,167,508,784],
-          label: "Latin America",
-          borderColor: "#e8c3b9",
-          fill: false
-        }, { 
-          data: [6,3,2,2,7,26,82,172,312,433],
-          label: "North America",
-          borderColor: "#c45850",
-          fill: false
         }
       ]
-    };
-    const myoptions = {
+    }; 
+    const elevationTimeOptions = {
       title: {
         display: true,
-        text: 'World population per region (in millions)'
+        text: 'Elevation Profile'
       },
+      // spanGaps: false,
       scales: {
         xAxes: [{
-          ticks: {
-            maxTicksLimit: 4
+          type: 'time',
+          time: {
+            displayFormats: {
+              quarter: 'HH:mm'
+            }
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Time'
+          }
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Elevation (m)'
           }
         }]
       },
+      events: [],
       elements: {
         point:{
           radius: 0
@@ -164,10 +116,174 @@ export class SettingsPage implements AfterViewInit {
       }
     };
 
-    var myLineChart = new Chart(lineCtx, {
+    var myLineChart = new Chart(elevationTimeCtx, {
       type: 'line',
       data: mydata,
-      options: myoptions
+      options: elevationTimeOptions
+    });
+
+    let speedCtx = this.speedTime.nativeElement.getContext('2d');
+  
+    const speeddata = {
+      datasets: [{ 
+          data: this.speedTimeData,
+          label: "Speed",
+          backgroundColor: 'rgba(255,133,0,0.2)',
+          borderColor: 'rgba(255,133,0,1)',
+          pointBackgroundColor: 'rgba(255,133,0,1)',
+          pointBorderColor: 'rgba(255,133,0,1)',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+      ]
+    }; 
+    const speedoptions = {
+      title: {
+        display: true,
+        text: 'Speed Profile'
+      },
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            displayFormats: {
+              quarter: 'HH:mm'
+            } 
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Time'
+          }
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Speed (km/h)'
+          }
+        }]
+      },
+      events: [],
+      elements: {
+        point:{
+          radius: 0
+        }
+      }
+    };
+
+    var speedChart = new Chart(speedCtx, {
+      type: 'line',
+      data: speeddata,
+      options: speedoptions
+    });
+
+
+    let elevationDistanceCtx = this.elevationDistance.nativeElement.getContext('2d');
+    
+    console.log(this.elevationDistanceData);
+    const elevationDistanceData = {
+      datasets: [{ 
+          data: this.elevationDistanceData,
+          label: "Elevation",
+          backgroundColor: 'rgba(255,133,0,0.2)',
+          borderColor: 'rgba(255,133,0,1)',
+          pointBackgroundColor: 'rgba(255,133,0,1)',
+          pointBorderColor: 'rgba(255,133,0,1)',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+      ]
+    }; 
+
+    const elevationDistanceOptions = {
+      title: {
+        display: true,
+        text: 'Elevation Profile'
+      },
+      // spanGaps: false,
+      scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Distance (kms)'
+          },
+          type: 'linear'
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Elevation (m)'
+          }
+        }]
+      },
+      events: [],
+      elements: {
+        point:{
+          radius: 0
+        },
+        line: {
+          tension: 0, // disables bezier curves
+        }
+      }
+    };
+
+    var elevationDistanceChart = new Chart(elevationDistanceCtx, {
+      type: 'line',
+      data: elevationDistanceData,
+      options: elevationDistanceOptions
+    });
+
+    let speedDistanceCtx = this.speedDistance.nativeElement.getContext('2d');
+  
+    console.log(this.speedDistanceData);
+    const speedDistanceData = {
+      datasets: [{ 
+          data: this.speedDistanceData,
+          label: "Speed",
+          backgroundColor: 'rgba(255,133,0,0.2)',
+          borderColor: 'rgba(255,133,0,1)',
+          pointBackgroundColor: 'rgba(255,133,0,1)',
+          pointBorderColor: 'rgba(255,133,0,1)',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+      ]
+    };
+
+    const speedDistanceOtions = {
+      title: {
+        display: true,
+        text: 'Speed Profile'
+      },
+      scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Distance (kms)'
+          },
+          type: 'linear'
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Speed (km/h)'
+          }
+        }]
+      },
+      events: [],
+      elements: {
+        point:{
+          radius: 0
+        },
+        line: {
+          tension: 0, // disables bezier curves
+        }
+      }
+    };
+
+    var speedDistanceChart = new Chart(speedDistanceCtx, {
+      type: 'line',
+      data: speedDistanceData,
+      options: speedDistanceOtions
     });
   }
 
@@ -179,9 +295,20 @@ export class SettingsPage implements AfterViewInit {
           result => {
             console.log(result);
             this.geoJsonObject = result.data.features[0];
-            this.lineData = result.data.features[0].speedsArr;
-            // this.createLineChart();
-            this.isProcessing = false;
+            this.elevationTimeData = result.data.features[0].geometry.coordinates.map((coord, i) => { return { x: result.data.timeArr[i], y: coord[2] } });
+            //reducing points on graph
+            this.elevationTimeData = this.elevationTimeData.filter((_,i) => i % 15 == 0); 
+            this.speedTimeData = result.data.features[0].speedsArr.map((speed, i) => { return { x: result.data.timeArr[i], y: speed } });
+            //reducing points on graph
+            this.speedTimeData = this.speedTimeData.filter((_,i) => i % 15 == 0); 
+            this.elevationDistanceData = result.data.features[0].geometry.coordinates.map((coord, i) => { return { x: result.data.distanceArr[i] / 1000, y: coord[2] } });
+            //reducing points on graph
+            this.elevationDistanceData = this.elevationDistanceData.filter((_,i) => i % 15 == 0); 
+            this.speedDistanceData = result.data.features[0].speedsArr.map((speed, i) => { return { x: result.data.distanceArr[i] / 1000, y: speed } });
+            //reducing points on graph
+            this.speedDistanceData = this.speedDistanceData.filter((_,i) => i % 15 == 0);
+            this.createLineChart();
+            this.isProcessing = false; 
           } 
         );
     }
